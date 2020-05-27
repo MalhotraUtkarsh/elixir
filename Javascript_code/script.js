2d0225919f48b33ab373d4ea53ac0f2d37dcec5e
MalhotraUtkarsh <MalhotraUtkarsh@users.noreply.github.com>
2020-05-27 19:59:51 +0530

    First Commit

---
+++ Javascript_code/script.js
@@ -0,0 +1,294 @@
+// Gallery Foods Function
+
+		function foods() {
+
+			//display : none;
+
+			document.getElementById('animate_1').style.opacity = "0" ;
+			document.getElementById('animate_1').style.transition = "all 2s" ;
+			document.getElementById('animate_2').style.opacity = "0" ;
+			document.getElementById('animate_2').style.transition = "all 2s" ;
+			document.getElementById('animate_3').style.opacity = "0" ;
+			document.getElementById('animate_3').style.transition = "all 2s" ;
+			document.getElementById('animate_5').style.opacity = "0" ;
+			document.getElementById('animate_5').style.transition = "all 2s" ;
+			document.getElementById('animate_8').style.opacity = "0" ;
+			document.getElementById('animate_8').style.transition = "all 2s" ;
+
+			// Translate 
+
+            document.getElementById('animate_0').style.opacity = "1" ;
+			document.getElementById('animate_4').style.transform = "translate3d(0px,-320px,0px)" ;
+			document.getElementById('animate_4').style.opacity = "1" ;
+			document.getElementById('animate_4').style.transition = "all 0.5s" ;
+			document.getElementById('animate_6').style.transform = "translate3d(910px,-640px,1150px)" ;
+			document.getElementById('animate_6').style.transition = "all 0.5s" ;
+			document.getElementById('animate_6').style.opacity = "1" ;
+			document.getElementById('animate_7').style.transform = "translate3d(-458px,-319px,-1150px)" ;
+			document.getElementById('animate_7').style.transition = "all 0.5s" ;
+			document.getElementById('animate_7').style.opacity = "1" ;
+		
+		}
+
+
+		function deserts() {
+
+
+			// Disappear
+
+			document.getElementById('animate_1').style.opacity = "0" ;
+			document.getElementById('animate_1').style.transition = "all 2s" ;
+			document.getElementById('animate_0').style.opacity = "0" ;
+			document.getElementById('animate_0').style.transition = "all 2s" ;
+			document.getElementById('animate_4').style.opacity = "0" ;
+			document.getElementById('animate_4').style.transition = "all 2s" ;
+			document.getElementById('animate_6').style.opacity = "0" ;
+			document.getElementById('animate_6').style.transition = "all 2s" ;
+			document.getElementById('animate_8').style.opacity = "0" ;
+			document.getElementById('animate_8').style.transition = "all 2s" ;
+
+
+
+
+			// Tranform
+
+
+			document.getElementById('animate_2').style.transform = "translate3d(-910px,0px,0px)" ;
+			document.getElementById('animate_2').style.opacity = "1" ;
+			document.getElementById('animate_2').style.transition = "all 0.5s" ;
+			document.getElementById('animate_3').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_3').style.opacity = "1" ;
+			document.getElementById('animate_3').style.transition = "all 0.5s" ;
+			document.getElementById('animate_5').style.transform = "translate3d(0px,-320px,0px)" ;
+			document.getElementById('animate_5').style.opacity = "1" ;
+			document.getElementById('animate_5').style.transition = "all 0.5s" ;
+			document.getElementById('animate_7').style.transform = "translate3d(0px,-640px,0px)" ;
+			document.getElementById('animate_7').style.transition = "all 0.5s" ; 
+
+
+
+		}
+
+		function all_images() {
+
+            document.getElementById('animate_0').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_0').style.opacity = "1" ;
+			document.getElementById('animate_0').style.transition = "all 0.5s" ;
+			document.getElementById('animate_1').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_1').style.opacity = "1" ;
+			document.getElementById('animate_1').style.transition = "all 0.5s" ;
+			document.getElementById('animate_2').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_2').style.opacity = "1" ;
+			document.getElementById('animate_2').style.transition = "all 0.5s" ;
+			document.getElementById('animate_3').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_3').style.opacity = "1" ;
+			document.getElementById('animate_3').style.transition = "all 0.5s" ;
+			document.getElementById('animate_4').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_4').style.opacity = "1" ;
+			document.getElementById('animate_4').style.transition = "all 0.5s" ;
+			document.getElementById('animate_5').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_5').style.opacity = "1" ;
+			document.getElementById('animate_5').style.transition = "all 0.5s" ;
+			document.getElementById('animate_6').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_6').style.opacity = "1" ;
+			document.getElementById('animate_6').style.transition = "all 0.5s" ;
+			document.getElementById('animate_7').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_7').style.opacity = "1" ;
+			document.getElementById('animate_7').style.transition = "all 0.5s" ;
+			document.getElementById('animate_8').style.transform = "translate3d(0px,0px,0px)" ;
+			document.getElementById('animate_8').style.opacity = "1" ;
+			document.getElementById('animate_8').style.transition = "all 0.5s" ;
+
+
+
+
+		}
+
+
+		function drinks() {
+
+
+
+			
+			document.getElementById('animate_0').style.opacity = "0" ;
+			document.getElementById('animate_0').style.transition = "all 0.5s" ;
+			
+			
+			document.getElementById('animate_3').style.opacity = "0" ;
+			document.getElementById('animate_3').style.transition = "all 0.5s" ;
+
+			
+			document.getElementById('animate_6').style.opacity = "0" ;
+			document.getElementById('animate_6').style.transition = "all 0.5s" ;
+			
+			document.getElementById('animate_7').style.opacity = "0" ;
+			document.getElementById('animate_7').style.transition = "all 0.5s" ;
+			
+			document.getElementById('animate_8').style.opacity = "0" ;
+			document.getElementById('animate_8').style.transition = "all 0.5s" ;
+
+
+
+			document.getElementById('animate_1').style.transform = "translate3d(-460px,0px,0px)" ;
+			document.getElementById('animate_1').style.opacity = "1" ;
+			document.getElementById('animate_1').style.transition = "all 0.5s" ;
+			document.getElementById('animate_2').style.transform = "translate3d(-460px,0px,0px)" ;
+			document.getElementById('animate_2').style.opacity = "1" ;
+			document.getElementById('animate_2').style.transition = "all 0.5s" ;
+
+
+			document.getElementById('animate_4').style.transform = "translate3d(-450px,0px,0px)" ;
+			document.getElementById('animate_4').style.opacity = "1" ;
+			document.getElementById('animate_4').style.transition = "all 0.5s" ;
+			document.getElementById('animate_5').style.transform = "translate3d(0px,-320px,0px)" ;
+			document.getElementById('animate_5').style.opacity = "1" ;
+			document.getElementById('animate_5').style.transition = "all 0.5s" ;
+
+		}
+
+	
+	//TESTIMONIALS SLIDE SHOW 
+
+ function testimonials_slideshow(n) {
+
+ 	if (n == 1) {
+ 		 document.getElementById('text_2').style.transform = "translate3d(1500px,-110px,0px)" ;
+        document.getElementById('text_2').style.transition = "all 2.1s ease-in-out" ;
+ 		document.getElementById('text_1').style.transform = "translate3d(0px,0px,0px)" ;
+ 		document.getElementById('text_1').style.transition = "all 2.5s ease-in-out" ;
+ 		document.getElementById('text_3').style.transform = "translate3d(1500px,-190px,0px)" ;
+ 		document.getElementById('text_3').style.transition = "all 1.5s ease-in-out" ;
+ 		// document.getElementById('testimonials_content').style.transition = "all 2s ease-in-out"
+
+ 	}
+ 	else if (n == 2) {
+ 		
+ 		document.getElementById('text_1').style.transform = "translate3d(-1500px,0px,0px)" ;
+ 		document.getElementById('text_1').style.transition = "all 2s ease-in-out" ;
+        document.getElementById('text_2').style.transform = "translate3d(0px,-110px,0px)" ;
+         document.getElementById('text_2').style.transition = "all 2s ease-in-out" ;
+ 		document.getElementById('text_3').style.transform= "translate3d(1500px,-190px,0px)" ;
+ 		document.getElementById('text_3').style.transition = "all 2s ease-in-out" ;
+
+ 	}
+ 	else if (n == 3) {
+       // document.getElementsByClassName('text_1').style.transform = "translate3d(-1500px,0px,0px)" ;
+        document.getElementById("text_1").style.transform = "translate3d(-2000px,20px,20px)"; 
+        document.getElementById('text_1').style.transition = "all 2s ease-in-out" ;
+        document.getElementById('text_2').style.transform = "translate3d(-1500px,-110px,0px)" ;
+        document.getElementById('text_2').style.transition = "all 2.5s ease-in-out" ;
+ 		document.getElementById('text_3').style.transform = "translate3d(0px,-190px,0px)" ;
+ 		document.getElementById('text_3').style.transition = "all 3s ease-in-out" ;
+ 
+
+ 	}
+
+ }
+
+	// Slide Show Begin
+
+  var myIndex = 1 ;
+  carousel()
+
+ function carousel(){ 
+ var i = 0 ;
+ var x = document.getElementsByClassName('img') ;
+ var y = document.getElementsByClassName('caption') ;
+
+ for (var i = 0; x.length > i; i++) {
+ 	x[i].style.opacity = "0" ;
+ 	y[i].style.transition = "transform 0.7s ease-in-out" ;
+    y[i].style.transform = "translateY(30px)";
+ 	y[i].style.opacity = "0" ;
+ }
+  myIndex++;
+  
+  if (myIndex > x.length) {myIndex = 1}   
+  x[myIndex-1].style.opacity = "0.4"; 
+  x[myIndex-1].style.transition = "all 0.7s ease-in-out" ;
+  y[myIndex-1].style.opacity = "1"; 
+  y[myIndex-1].style.transform = "translateY(-30px)";
+  y[myIndex-1].style.transition = "all 0.7s ease-in-out" 
+  
+  setTimeout(carousel, 3000)
+
+}
+
+
+// History Slide Show
+
+history()
+var history_index = 0
+function history(){
+
+ var i = 0 ;
+ var x = document.getElementsByClassName('about_img_history') ;
+
+ for (var i = 0; x.length > i; i++) {
+ 	x[i].style.opacity = "0" ;
+ 	x[i].style.transition = "opacity 0.7s ease-in-out" ;
+ }
+  history_index ++;
+  
+  if (history_index > x.length) {history_index = 1}   
+  x[myIndex-1].style.opacity = "1"; 
+  x[myIndex-1].style.transition = "all 0.7s ease-in-out" ; 
+  
+  setTimeout(history, 1000)
+
+}
+
+// HISTORY SLIDING SHOW 
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+
+	var i;
+	var slides = document.getElementsByClassName('about_img_history_2') ;
+
+	if (n > slides.length) {slideIndex = 1}  ;  
+    if (n < 1) {slideIndex = slides.length} ;
+
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none' ;
+	}
+
+	slides[slideIndex-1].style.display = 'block';
+
+
+
+/*  var i;
+  var slides = document.getElementsByClassName("about_img_history_2");
+  // var dots = document.getElementsByClassName("dots");
+  
+  // KONSI SLIDE DIKANI HAI
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  
+  // SABHI SLIDE  KO DISAPPEAR KARNA
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  // DOTS
+  /* for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+   */
+  //SLIDE KO DIKAYGA 
+  
+ // slides[slideIndex-1].style.display = "block";  
+
+ //  dots[slideIndex-1].className += " active";
+
+}
